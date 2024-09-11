d9c797101686c7ece334ab58a125da5d4e7455b4
tkazDeveloper <87867785+tkazDeveloper@users.noreply.github.com>
2024-09-11 11:52:21 +0900

    2nd Commit

--- PerfumeClosetList/flutter_service_worker.js
+++ PerfumeClosetList/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "f59b4c3584fd089c5d3f210ff8121528",
-"index.html": "5b6ff58b390373e5cf891c32fe253a6f",
-"/": "5b6ff58b390373e5cf891c32fe253a6f",
-"main.dart.js": "d49902501620b1106fe35bc41fb7c8fe",
+"index.html": "7b4d25bc4e6a49ef9f4d9af0265bd289",
+"/": "7b4d25bc4e6a49ef9f4d9af0265bd289",
+"main.dart.js": "25dbc16bdf9c1baf3f514e286601ed75",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
